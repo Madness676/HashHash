46b930f55c6ac1f8d52ffeca2480098bfae6ae2e
Madness676 <117950457+Madness676@users.noreply.github.com>
2024-04-15 20:05:23 +0800

    Update change.java

--- change.java
+++ change.java
@@ -11,8 +11,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String githubToken = args[0];
         String gistId = args[1];
-        String username = "MADNESSTEST";
-        String repositoryName = "hashhash";
+        String username = "Madness676";
+        String repositoryName = "HashHash";
         String filePath = "new.txt";
 
         String latestCommitHash = getLatestGistCommitHash(gistId);
